46cbbb2f5932e907fc55a40dc780d92d00d7caf7
NicolasAmadori <75174666+NicolasAmadori@users.noreply.github.com>
2021-02-16 09:14:28 +0100

    Add files via upload

---
+++ wifiClient.ino
@@ -0,0 +1,42 @@
+#include "WiFi.h"
+
+const char* ssid = "password";
+const char* password = "password";
+
+const uint16_t port = 60101;
+const char * host = "192.168.0.2";
+
+void setup()
+{
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+ 
+  Serial.print("WiFi connected with IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+  WiFiClient client;
+ 
+    if (!client.connect(host, port)) {
+ 
+        Serial.println("Connection to host failed");
+ 
+        delay(1000);
+        return;
+    }
+ 
+    Serial.println("Connected to server successful!");
+ 
+    client.print("Hello from ESP32!");
+ 
+    Serial.println("Disconnecting...");
+    client.stop();
+ 
+    delay(10000);
+}
